2ec68ef1de63928334a65c4cf0d18bc2201a422e
Vincent Ahrend <vincent.ahrend@symbolian.net>
2016-06-13 13:33:53 +0200

    Bundle and minify Javascript

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-echo "Building purescript-conecanvas..."
-cd ../purescript-OntoPanel
-pulp build &&
-psc-bundle -m Main output/**/*.js -o ../cone-reddit/html/js/Main.js &&
-cd ../cone-reddit &&
-
 cabal run -- "$@"
