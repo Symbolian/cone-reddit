4823379d6f0901d943fb59ce5339567f336bdac9
Michael Karg <mgoremeier@gmail.com>
2016-06-20 15:19:08 +0200

    distinguish devel/live config

--- src/Config.hs
+++ src/Config.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, CPP #-}
 
 module Config where
 
@@ -9,33 +9,50 @@ import Data.Text                                (Text)
 
 -- base directory of cone for reddit
 baseDir :: FilePath
-baseDir = "/Users/work/code/cc-reddit/"
+
+-- cone server port
+srvPort :: Port
+
+-- how often is new data is crawled on reddit (in minutes)
+updateInterval :: Int
+
+
+#ifndef RELEASE
+
+-- DEVELOP config
+baseDir         = "/Users/work/code/cc-reddit/"
+srvPort         = 8080
+updateInterval  = 15
+
+#else
+
+-- LIVE config
+baseDir         = "/var/www/"
+srvPort         = 80
+updateInterval  = 15
+
+#endif
+
+
 
 -- location at which serialised data is stored
 dataFileName :: FilePath
 dataFileName = "cone.model"
 
--- cone server port
-srvPort :: Port
-srvPort = 8080
 
 -- show this in upper left hand corner of conecanvas
 domainLabel :: Text
 domainLabel = "Reddit"
 
--- How often is new data added (in minutes)
-updateInterval :: Int
-updateInterval = 15
-
 -- Reddit API requires a custo user agent string, which is why these options
 -- need to be specified instead of using the defaults.
 redditOptions :: RedditOptions
-redditOptions = RedditOptions {
-    rateLimitingEnabled     = True,
-    connectionManager       = Nothing,
-    loginMethod             = Anonymous,
-    customUserAgent         = Just "symbolian.cone:v.1.0.0 (by /u/RelevantBits)"
-}
+redditOptions = RedditOptions
+    { rateLimitingEnabled   = True
+    , connectionManager     = Nothing
+    , loginMethod           = Anonymous,
+    , customUserAgent       = Just "symbolian.cone:v.1.0.0 (by /u/RelevantBits)"
+    }
 
 -- subreddits to be considered
 subredditNames :: [SubredditName]
